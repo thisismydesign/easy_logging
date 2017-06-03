d4ab133ef002b2237a054c4e3be55572d01e7dfb
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-03 21:11:55 +0200

    Moved test classes into testsuite; Rearranged test cases; Added test cases: logging to STDOUT, logger contains and outputs class name, logger is specific to class

--- spec/easy_logging_spec.rb
+++ spec/easy_logging_spec.rb
@@ -1,21 +1,65 @@
 require "spec_helper"
 
-require_relative 'test_class'
+class TestClass
+  include EasyLogging
+end
+
+class TestClass2
+  include EasyLogging
+end
 
 RSpec.describe EasyLogging do
   it "has a version number" do
     expect(EasyLogging::VERSION).not_to be nil
   end
 
-  context "an instance of a class using EasyLogging" do
-    it "has a logger field" do
+  context "instance of a class that includes EasyLogging" do
+    it "has a logger" do
       expect(TestClass.new.respond_to?(:logger)).to be(true)
     end
   end
 
-  context "a class using EasyLogging" do
-    it "has a logger field" do
+  context "class that includes EasyLogging" do
+    it "has a logger" do
       expect(TestClass.respond_to?(:logger)).to be(true)
+    end
+  end
+
+  context "instance level logger" do
+    it "contains class name" do
+      expect(TestClass.logger.progname).not_to eq(TestClass.class.name)
+    end
+
+    it "outputs class name" do
+      expect { TestClass.new.logger.info }.to output(/.+#{TestClass.class.name}.+/).to_stdout_from_any_process
+    end
+
+    it "can log to STDOUT" do
+      msg = "hi"
+      expect { TestClass.new.logger.info(msg) }.to output(/.+#{msg}$/).to_stdout_from_any_process
+    end
+
+    it "is specific to the class" do
+      expect(TestClass.new.logger.__id__).not_to eq(TestClass2.new.logger.__id__)
+    end
+  end
+
+  context "class level logger" do
+    it "contains class name" do
+      expect(TestClass.logger.progname).not_to eq(TestClass.class.name)
+    end
+    
+    it "outputs class name" do
+      expect { TestClass.logger.info }.to output(/.+#{TestClass.class.name}.+/).to_stdout_from_any_process
+    end
+
+    it "can log to STDOUT" do
+      msg = "hi"
+      expect { TestClass.logger.info(msg) }.to output(/.+#{msg}$/).to_stdout_from_any_process
+    end
+
+    it "is specific to the class" do
+      expect(TestClass.logger.__id__).not_to eq(TestClass2.logger.__id__)
     end
   end
 
