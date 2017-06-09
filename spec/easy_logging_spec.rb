a734bf488cc515143ecb3dc9e14a8bd10ad2aa5f
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-09 19:11:01 +0200

    Fix issue with ENV being changed; Add several test cases related to 'selective output destination'

--- spec/easy_logging_spec.rb
+++ spec/easy_logging_spec.rb
@@ -61,7 +61,6 @@ RSpec.describe EasyLogging do
       expect { TestClass.logger.info(msg) }.to output(/.+#{msg}$/).to_stdout_from_any_process
     end
 
-
     it "is specific to the class" do
       expect(TestClass.logger.__id__).not_to eq(TestClass2.logger.__id__)
     end
@@ -69,31 +68,83 @@ RSpec.describe EasyLogging do
 
   context 'selective output destination' do
 
-    let(:log_file) { Tempfile.new('easy_logging') }
-    let(:easy_clone) { EasyLogging.clone }
-
-    before :each do
-      stub_const('ENV',mocked_env_with('LOGFILE'=>log_file.path))
-      class TestDestClass;end
-      TestDestClass.send(:include, easy_clone)
+    it "logs to STDOUT by default" do
+      expect(get_device(TestClass.logger).inspect.include?("STDOUT")).to be true
     end
 
-    after :each do
-      log_file.close
-      log_file.unlink
-    end
-
-    it 'should have correct log file' do
+    it 'remembers selected output destination' do
+      easy_clone = EasyLogging.clone
       easy_clone.log_destination = 'test'
       expect(easy_clone.log_destination).to eq 'test'
     end
 
+    it "reads log destination from `LOGFILE` environment variable if available" do
+      logfile_path = 'easy_logging'
+      logfile_env_entry = {'LOGFILE'=>logfile_path}
+      stub_const('ENV',mocked_env_with(logfile_env_entry))
+      easy_clone = EasyLogging.clone
 
-    it "can log to file" do
-        msg = 'hi'
-        expect(easy_clone.log_destination).to eq log_file.path
-        TestDestClass.logger.info(msg)
-        expect(log_file.read).to match(/.+#{msg}$/)
+      expect(easy_clone.log_destination).to eq logfile_path
+    end
+
+    context 'messing with log_destination settings directly' do
+
+      after :each do
+        EasyLogging.log_destination = nil
+      end
+
+      context 'logging to files' do
+
+        let(:log_file) { Tempfile.new('easy_logging') }
+
+        after :each do
+          log_file.close
+          log_file.unlink
+        end
+
+        it "can log to file" do
+          EasyLogging.log_destination = log_file.path
+          class TestLogFile;end
+          TestLogFile.send(:include, EasyLogging)
+
+          msg = 'hi'
+          TestLogFile.logger.info(msg)
+          expect(log_file.read).to match(/.+#{msg}$/)
+        end
+
+        context "on the fly modification of `log_destination`" do
+
+          it "uses new log_destination in every logger even already created ones" do
+            old_log = STDOUT
+            new_log = log_file.path
+
+            EasyLogging.log_destination = old_log
+            class TestLogChange;end
+            TestLogChange.send(:include, EasyLogging)
+
+            class TestLogChange2;end
+            EasyLogging.log_destination = new_log
+            TestLogChange2.send(:include, EasyLogging)
+
+            expect(get_device(TestLogChange2.logger).path).to eq(new_log)
+            expect(get_device(TestLogChange.logger).path).to eq(new_log)
+          end
+
+        end
+
+        it "retains `log_destination` between includes" do
+          log = log_file.path
+          EasyLogging.log_destination = log
+          class TestRetain;end
+          TestRetain.send(:include, EasyLogging)
+
+          class TestRetain2;end
+          TestRetain2.send(:include, EasyLogging)
+          
+          expect(get_device(TestRetain.logger).path).to eq(log)
+          expect(get_device(TestRetain2.logger).path).to eq(log)
+        end
+      end
     end
   end
 end
--- spec/spec_support.rb
+++ spec/spec_support.rb
@@ -1,11 +1,11 @@
 module SpecSupport
 
   def mocked_env_with(dict)
-    clone = ENV.clone
-    dict.each_pair do |k,v|
-      clone[k]=v
-    end
-    clone
+    ENV.to_h.merge(dict)
+  end
+
+  def get_device(logger)
+    logger.instance_variable_get(:@logdev).instance_variable_get(:@dev)
   end
 
 end
