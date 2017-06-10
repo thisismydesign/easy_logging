4a5822c8782d94fe23104847fed16a0344e04278
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-10 17:19:15 +0200

    Add test cases for on the fly modification of logger configuration

--- spec/easy_logging_spec.rb
+++ spec/easy_logging_spec.rb
@@ -112,24 +112,116 @@ RSpec.describe EasyLogging do
           expect(log_file.read).to match(/.+#{msg}$/)
         end
 
-        context "on the fly modification of `log_destination`" do
+        context "on the fly modification of logger configuration" do
 
-          it "uses new log_destination in every logger even already created ones" do
-            old_log = STDOUT
-            new_log = log_file.path
+          let(:old_log) { STDOUT }
+          let(:new_log) { log_file.path }
 
-            EasyLogging.log_destination = old_log
-            class TestLogChange;end
-            TestLogChange.send(:include, EasyLogging)
+          context "modification of `EasyLogging.log_destination`" do
+            it "uses new log_destination in new and not yet used loggers" do
+              EasyLogging.log_destination = old_log
+              class TestDirectLogChange3;end
+              TestDirectLogChange3.send(:include, EasyLogging)
 
-            class TestLogChange2;end
-            EasyLogging.log_destination = new_log
-            TestLogChange2.send(:include, EasyLogging)
+              class TestDirectLogChange4;end
+              EasyLogging.log_destination = new_log
+              TestDirectLogChange4.send(:include, EasyLogging)
 
-            expect(get_device(TestLogChange2.logger).path).to eq(new_log)
-            expect(get_device(TestLogChange.logger).path).to eq(new_log)
+              expect(get_device(TestDirectLogChange4.logger).path).to eq(new_log)
+              expect(get_device(TestDirectLogChange3.logger).path).to eq(new_log)
+            end
+
+            # TODO this is unexplained behaviour
+            it "uses old log_destination in already used loggers" do
+              EasyLogging.log_destination = old_log
+              class TestDirectLogChange;end
+              TestDirectLogChange.send(:include, EasyLogging)
+
+              # 'Use' logger
+              TestDirectLogChange.logger
+
+              class TestDirectLogChange2;end
+              EasyLogging.log_destination = new_log
+              TestDirectLogChange2.send(:include, EasyLogging)
+
+              expect(get_device(TestDirectLogChange2.logger).path).to eq(new_log)
+              expect(get_device(TestDirectLogChange.logger).inspect.include?("STDOUT")).to be true
+            end
+
           end
 
+          context "modification of environment variable" do
+
+            it "uses new log_destination in new and not yet used loggers" do
+              logfile_env_entry = {'LOGFILE'=>old_log}
+              stub_const('ENV',mocked_env_with(logfile_env_entry))
+
+              class TestEnvChange;end
+              TestEnvChange.send(:include, EasyLogging)
+
+              logfile_env_entry = {'LOGFILE'=>new_log}
+              stub_const('ENV',mocked_env_with(logfile_env_entry))
+
+              class TestEnvChange2;end
+              TestEnvChange2.send(:include, EasyLogging)
+
+              expect(get_device(TestEnvChange2.logger).path).to eq(new_log)
+              expect(get_device(TestEnvChange.logger).path).to eq(new_log)
+            end
+
+            # TODO this is unexplained behaviour
+            it "uses old log_destination in already used loggers" do
+              logfile_env_entry = {'LOGFILE'=>old_log}
+              stub_const('ENV',mocked_env_with(logfile_env_entry))
+
+              class TestEnvChange3;end
+              TestEnvChange3.send(:include, EasyLogging)
+
+              # 'Use' logger
+              TestEnvChange3.logger
+
+              logfile_env_entry = {'LOGFILE'=>new_log}
+              stub_const('ENV',mocked_env_with(logfile_env_entry))
+
+              class TestEnvChange4;end
+              TestEnvChange4.send(:include, EasyLogging)
+
+              expect(get_device(TestEnvChange4.logger).path).to eq(new_log)
+              expect(get_device(TestEnvChange3.logger).inspect.include?("STDOUT")).to be true
+            end
+          end
+
+          context "modification of both `EasyLogging.log_destination` and environment variable" do
+            it "modification of `EasyLogging.log_destination` will overwrite environment variable setting" do
+              logfile_env_entry = {'LOGFILE'=>old_log}
+              stub_const('ENV',mocked_env_with(logfile_env_entry))
+
+              class TestDirectAndEnvChange;end
+              TestDirectAndEnvChange.send(:include, EasyLogging)
+
+              EasyLogging.log_destination = new_log
+              class TestDirectAndEnvChange2;end
+              TestDirectAndEnvChange2.send(:include, EasyLogging)
+
+              expect(get_device(TestDirectAndEnvChange2.logger).path).to eq(new_log)
+              expect(get_device(TestDirectAndEnvChange.logger).path).to eq(new_log)
+            end
+
+            it "modification of environment variable will NOT overwrite `EasyLogging.log_destination` setting" do
+              EasyLogging.log_destination = old_log
+              class TestDirectAndEnvChange3;end
+              TestDirectAndEnvChange3.send(:include, EasyLogging)
+
+              logfile_env_entry = {'LOGFILE'=>new_log}
+              stub_const('ENV',mocked_env_with(logfile_env_entry))
+              class TestDirectAndEnvChange4;end
+              TestDirectAndEnvChange4.send(:include, EasyLogging)
+
+              expect(get_device(TestDirectAndEnvChange4.logger).inspect.include?("STDOUT")).to be true
+              expect(get_device(TestDirectAndEnvChange3.logger).inspect.include?("STDOUT")).to be true
+            end
+          end
+          
         end
 
         it "retains `log_destination` between includes" do
