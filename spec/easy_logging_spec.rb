0f75b6ce52562ba84caebd25d841a29e1c152f4d
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-08-19 15:56:28 +0200

    Replaces " with '

--- spec/easy_logging_spec.rb
+++ spec/easy_logging_spec.rb
@@ -1,8 +1,8 @@
-require "spec_helper"
+require 'spec_helper'
 require 'tempfile'
 
 RSpec.describe EasyLogging do
-  it "has a version number" do
+  it 'has a version number' do
     expect(EasyLogging::VERSION).not_to be nil
   end
 
@@ -16,60 +16,60 @@ RSpec.describe EasyLogging do
     end
   end
 
-  describe "instance of a class that includes EasyLogging" do
-    it "has a logger" do
+  describe 'instance of a class that includes EasyLogging' do
+    it 'has a logger' do
       expect(TestClass.new.respond_to?(:logger)).to be(true)
     end
   end
 
-  describe "class that includes EasyLogging" do
-    it "has a logger" do
+  describe 'class that includes EasyLogging' do
+    it 'has a logger' do
       expect(TestClass.respond_to?(:logger)).to be(true)
     end
   end
 
-  describe "instance level logger" do
-    it "contains class name" do
+  describe 'instance level logger' do
+    it 'contains class name' do
       expect(TestClass.logger.progname).not_to eq(TestClass.class.name)
     end
 
-    it "outputs class name" do
+    it 'outputs class name' do
       expect { TestClass.new.logger.info }.to output(/.+#{TestClass.class.name}.+/).to_stdout_from_any_process
     end
 
-    it "can log to STDOUT" do
-      msg = "hi"
+    it 'can log to STDOUT' do
+      msg = 'hi'
       expect { TestClass.new.logger.info(msg) }.to output(/.+#{msg}$/).to_stdout_from_any_process
     end
 
-    it "is specific to the class" do
+    it 'is specific to the class' do
       expect(TestClass.new.logger.__id__).not_to eq(TestClass2.new.logger.__id__)
     end
   end
 
-  describe "class level logger" do
-    it "contains class name" do
+  describe 'class level logger' do
+    it 'contains class name' do
       expect(TestClass.logger.progname).not_to eq(TestClass.class.name)
     end
 
-    it "outputs class name" do
+    it 'outputs class name' do
       expect { TestClass.logger.info }.to output(/.+#{TestClass.class.name}.+/).to_stdout_from_any_process
     end
 
-    it "can log to STDOUT" do
-      msg = "hi"
+    it 'can log to STDOUT' do
+      msg = 'hi'
       expect { TestClass.logger.info(msg) }.to output(/.+#{msg}$/).to_stdout_from_any_process
     end
 
-    it "is specific to the class" do
+    it 'is specific to the class' do
       expect(TestClass.logger.__id__).not_to eq(TestClass2.logger.__id__)
     end
   end
 
   describe 'output destination selection' do
 
-    it "logs to STDOUT by default" do
-      expect(get_device(TestClass.logger).inspect.include?("STDOUT")).to be true
+    it 'logs to STDOUT by default' do
+      expect(get_device(TestClass.logger).inspect.include?('STDOUT')).to be true
     end
 
     it 'remembers selected output destination' do
@@ -93,7 +93,7 @@ RSpec.describe EasyLogging do
           log_file.unlink
         end
 
-        it "can log to file" do
+        it 'can log to file' do
           EasyLogging.log_destination = log_file.path
           class TestLogFile;end
           TestLogFile.send(:include, EasyLogging)
@@ -103,7 +103,7 @@ RSpec.describe EasyLogging do
           expect(log_file.read).to match(/.+#{msg}$/)
         end
 
-        it "retains `log_destination` between includes" do
+        it 'retains `log_destination` between includes' do
           log = log_file.path
           EasyLogging.log_destination = log
           class TestDestinationRetain; end
@@ -116,13 +116,13 @@ RSpec.describe EasyLogging do
           expect(get_device(TestDestinationRetain2.logger).path).to eq(log)
         end
 
-        context "on the fly modification of logger configuration" do
+        context 'on the fly modification of logger configuration' do
 
           let(:old_log) { STDOUT }
           let(:new_log) { log_file.path }
 
-          context "modification of `EasyLogging.log_destination`" do
-            it "uses new log_destination in new and not yet used loggers" do
+          context 'modification of `EasyLogging.log_destination`' do
+            it 'uses new log_destination in new and not yet used loggers' do
               EasyLogging.log_destination = old_log
               class TestDirectLogChange;end
               TestDirectLogChange.send(:include, EasyLogging)
@@ -136,7 +136,7 @@ RSpec.describe EasyLogging do
             end
 
             # TODO this is unexplained behaviour
-            it "uses old log_destination in already used loggers" do
+            it 'uses old log_destination in already used loggers' do
               EasyLogging.log_destination = old_log
               class TestDirectLogChange3;end
               TestDirectLogChange3.send(:include, EasyLogging)
@@ -149,7 +149,7 @@ RSpec.describe EasyLogging do
               TestDirectLogChange4.send(:include, EasyLogging)
 
               expect(get_device(TestDirectLogChange4.logger).path).to eq(new_log)
-              expect(get_device(TestDirectLogChange3.logger).inspect.include?("STDOUT")).to be true
+              expect(get_device(TestDirectLogChange3.logger).inspect.include?('STDOUT')).to be true
             end
           end
         end
