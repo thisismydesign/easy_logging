b414d5d29d101da51acb6722d19b16a5d26e6233
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-08-19 18:04:32 +0200

    Removes dead code

--- spec/spec_support.rb
+++ spec/spec_support.rb
@@ -1,9 +1,5 @@
 module SpecSupport
 
-  def mocked_env_with(dict)
-    ENV.to_h.merge(dict)
-  end
-
   def get_device(logger)
     logger.instance_variable_get(:@logdev).instance_variable_get(:@dev)
   end
